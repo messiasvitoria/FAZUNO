1b3077c10f9ea55dd1909c228d1d1ee693529507
messias-18 <mariaclaramessias82@gmail.com>
2026-06-14 02:41:13 -0300

    Atualizanso Tela_Calendario_Prestador

---
+++ app/layout.tsx
@@ -0,0 +1,33 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "FazUno",
+  description: "Tudo o que sua casa precisa em um só lugar",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      lang="pt-BR"
+      className={`${geistSans.variable} ${geistMono.variable} antialiased overflow-hidden h-full`}
+    >
+      <body className="overflow-hidden h-full w-full">{children}</body>
+    </html>
+  );
+}
