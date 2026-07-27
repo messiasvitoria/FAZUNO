d4d65d2038c38064e8e46dd2de2b8bfdb431620a
unknown <alicereginadasilvamonteiro@gmail.com>
2026-07-26 21:24:04 -0300

    Atualiza tela de cadastro de serviço do prestador

--- app/Pages/Tela_CadastroServico_Prestador/page.jsx
+++ app/Pages/Tela_CadastroServico_Prestador/page.jsx
@@ -1,10 +1,5 @@
 import Tela_CadastroServico_Prestador from "./Tela_CadastroServico_Prestador";
-import PrestadorLayout from "../../components/PrestadorLayout";
 
 export default function Page() {
-  return (
-    <PrestadorLayout title="Adicionar Serviço" subtitle="Cadastre um novo serviço para seus clientes.">
-      <Tela_CadastroServico_Prestador />
-    </PrestadorLayout>
-  );
+  return <Tela_CadastroServico_Prestador />;
 }
